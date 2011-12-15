f2cb0159b77970c6baeb2e7f7e208febba2bbfe9
Mariana Mărășoiu <mariana.marasoiu@gmail.com>
2011-12-15 02:52:42 +0200

    Add sql related functions. Needs revision.

--- base.py
+++ base.py
@@ -69,6 +69,36 @@ class DatabaseFeatures(BaseDatabaseFeatures):
 class DatabaseOperations(BaseDatabaseOperations):
     compiler_module = "django.db.backends.cubrid.compiler"
 
+    def date_extract_sql(self, lookup_type, field_name):
+        if lookup_type == 'week_day':
+            # DAYOFWEEK() returns an integer, 1-7, Sunday=1.
+            # Note: WEEKDAY() returns 0-6, Monday=0.
+            return "DAYOFWEEK(%s)" % field_name
+        else:
+            return "EXTRACT(%s FROM %s)" % (lookup_type.upper(), field_name)
+
+    def date_trunc_sql(self, lookup_type, field_name):
+        fields = ['year', 'month', 'day', 'hour', 'minute', 'second', 'milisecond']
+        format = ('%%Y-', '%%m', '-%%d', ' %%H:', '%%i', ':%%s', '.%%ms') # Use double percents to escape.
+        format_def = ('0000-', '01', '-01', ' 00:', '00', ':00', '.00')
+        try:
+            i = fields.index(lookup_type) + 1
+        except ValueError:
+            sql = field_name
+        else:
+            format_str = ''.join([f for f in format[:i]] + [f for f in format_def[i:]])
+            sql = "CAST(DATE_FORMAT(%s, '%s') AS DATETIME)" % (field_name, format_str)
+        return sql
+
+    def drop_foreignkey_sql(self):
+        return "DROP FOREIGN KEY"
+
+    def force_no_ordering(self):
+        return ["NULL"]
+
+    def fulltext_search_sql(self, field_name):
+        return 'MATCH (%s) AGAINST (%%s IN BOOLEAN MODE)' % field_name
+
     def quote_name(self, name):
         if name.startswith("`") and name.endswith("`"):
             return name # Quoting once is enough.
@@ -83,9 +113,63 @@ class DatabaseOperations(BaseDatabaseOperations):
         result = cursor.fetchone()
         return result[0]
 
-    # TODO: Implement methods covering CUBRID-specific characteristics.
-    # Start with methods returning NotImplementedError().
+    def random_function_sql(self):
+        return 'RAND()'
 
+    def sql_flush(self, style, tables, sequences):
+        # NB: The generated SQL below is specific to MySQL
+        # 'TRUNCATE x;', 'TRUNCATE y;', 'TRUNCATE z;'... style SQL statements
+        # to clear all tables of all data
+        print "sqlflush"
+        if tables:
+#            sql = ['SET FOREIGN_KEY_CHECKS = 0;']
+            sql = []
+            for table in tables:
+                sql.append('%s %s;' % (style.SQL_KEYWORD('TRUNCATE'), style.SQL_FIELD(self.quote_name(table))))
+#            sql.append('SET FOREIGN_KEY_CHECKS = 1;')
+
+            # 'ALTER TABLE table AUTO_INCREMENT = 1;'... style SQL statements
+            # to reset sequence indices
+            sql.extend(["%s %s %s %s %s;" % \
+                (style.SQL_KEYWORD('ALTER'),
+                 style.SQL_KEYWORD('TABLE'),
+                 style.SQL_TABLE(self.quote_name(sequence['table'])),
+                 style.SQL_KEYWORD('AUTO_INCREMENT'),
+                 style.SQL_FIELD('= 1'),
+                ) for sequence in sequences])
+            print sql
+            return sql
+        else:
+            return []
+
+    def value_to_db_datetime(self, value):
+        if value is None:
+            return None
+
+        #TODO: Check if Cubrid supports timezones
+        if value.tzinfo is not None:
+            raise ValueError("Timezone-aware datetime not implemented yet.")
+
+        return unicode(value)
+
+    def value_to_db_time(self, value):
+        if value is None:
+            return None
+
+        #TODO: Check if Cubrid supports timezones
+        if value.tzinfo is not None:
+            raise ValueError("Timezone-aware datetime not implemented yet.")
+
+        return unicode(value)
+
+    def year_lookup_bounds(self, value):
+        # Again, no microseconds
+        first = '%s-01-01 00:00:00.00'
+        second = '%s-12-31 23:59:59.99'
+        return [first % value, second % value]
+
+    def max_name_length(self):
+        return 64
 
 class DatabaseWrapper(BaseDatabaseWrapper):
     vendor = 'cubrid'
